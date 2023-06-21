7372fbac274a73c4cd00786e1a19dff6a69b6f9d
Faisal Fagihi <fai9al.sa@gmail.com>
2023-06-21 23:18:10 +0300

    Continue Initial Implementation

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -17,16 +17,16 @@ const firebaseConfig = {
  firebase.initializeApp(firebaseConfig)
  const messaging = firebase.messaging();
 
-//  messaging.onMessage((payload) => {
-//   // GlobalNotificationService.showNotification(payload?.notification?.title)
+ messaging.onMessage((payload) => {
+  // GlobalNotificationService.showNotification(payload?.notification?.title)
 
-//    self.registration.showNotification(payload?.notification?.title,
-//     {
-//       body: payload?.notification?.body,
-//       icon:"https://lh3.googleusercontent.com/u/0/drive-viewer/AFGJ81oBe4w5efa7Omi3PdQR74_Q2LfSf8po9SrGE5FMstI4wIcbeBupKtYw3TMlQiMxF42p1ULGK3hxh5g6Yo7YLccrJ6wlRw=w1278-h1279",
-//     })
+   self.registration.showNotification(payload?.notification?.title,
+    {
+      body: payload?.notification?.body,
+      icon:"../dist/assets/windows11/Square44x44Logo.scale-200.png"
+    })
 
-// });
+});
 
 //  messaging.onBackgroundMessage((payload) => {
 //     // Customize notification here
